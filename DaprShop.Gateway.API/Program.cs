32885ae04b53c9fde6337014c6a9e79bb91a56fa
William Liebenberg <awliebenberg@outlook.com>
2023-04-10 07:59:46 +1000

    fix for swagger to respect `url.primaryName` query param

--- DaprShop.Gateway.API/Program.cs
+++ DaprShop.Gateway.API/Program.cs
@@ -1,18 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 
-using Yarp.ReverseProxy.Transforms;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddReverseProxy()
-    //.AddTransforms(builderContext =>
-    //{
-    //    builderContext.CopyRequestHeaders = true;
-    //    builderContext.AddOriginalHost(useOriginal: true);
-    //    builderContext.UseDefaultForwarders = true;
-    //})
-    //.AddApiGatewayConfiguration(builder.Configuration);    
     .LoadFromConfig(builder.Configuration.GetSection("DaprReverseProxy"));
 
 builder.Services.AddEndpointsApiExplorer();
@@ -30,8 +21,14 @@ app.UseSwagger(options =>
 app.UseSwaggerUI(setup =>
 {
     setup.RoutePrefix = string.Empty;
-    setup.SwaggerEndpoint("swagger/v1/swagger.json", "Api Gateway");
-    
+    setup.SwaggerEndpoint("swagger/v1/swagger.json", "Api Gateway");    
+    setup.ConfigObject.DisplayRequestDuration = true;
+
+    // both flags work to enable the `url.primaryName` query parameter to be respected
+    // see: https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/2516
+    setup.ConfigObject.DeepLinking = true;
+    setup.ConfigObject.AdditionalItems["queryConfigEnabled"] = true;
+
     IConfigurationSection apiRoutesSection = builder.Configuration.GetSection("ApiRoutes");
     ApiRouteConfig[]? apiRoutes = apiRoutesSection.Get<ApiRouteConfig[]>();
     if(apiRoutes != null)
@@ -51,11 +48,7 @@ app.MapGet("info", ([FromServices] IConfiguration config) =>
     ApiRouteConfig[]? routes = apiRoutesSection.Get<ApiRouteConfig[]>();
     return Task.FromResult(Results.Ok(routes));
 })
-            .WithOpenApi()
-            .WithName("Info");
-
-
-//app.UseRouting();
-//app.UseCors();
+    .WithOpenApi()
+    .WithName("Info");
 
 app.Run();
