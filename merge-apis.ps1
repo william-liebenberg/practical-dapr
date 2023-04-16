d9e3ba151fa616967be37a70dd80f69768981510
William Liebenberg <awliebenberg@outlook.com>
2023-04-17 00:27:03 +1000

    add script to merge API specs into final API Gateway spec

---
+++ merge-apis.ps1
@@ -0,0 +1,61 @@
+function Join-Objects($source, $extend) {
+    if ($source.GetType().Name -eq "PSCustomObject" -and $extend.GetType().Name -eq "PSCustomObject") {
+        foreach ($Property in $source | Get-Member -type NoteProperty, Property) {
+            if ($null -eq $extend.$($Property.Name)) {
+                continue;
+            }
+            $source.$($Property.Name) = Join-Objects $source.$($Property.Name) $extend.$($Property.Name)
+        }
+    }
+    else {
+        $source = $extend;
+    }
+    return $source
+}
+
+function Add-Property($source, $toExtend) {
+    if ($source.GetType().Name -eq "PSCustomObject") {
+        foreach ($Property in $source | Get-Member -type NoteProperty, Property) {
+            if ($null -eq $toExtend.$($Property.Name)) {
+                $toExtend | Add-Member -MemberType NoteProperty -Value $source.$($Property.Name) -Name $Property.Name
+            }
+            else {
+                $toExtend.$($Property.Name) = Add-Property $source.$($Property.Name) $toExtend.$($Property.Name)
+            }
+        }
+    }
+    return $toExtend
+}
+
+function Merge-Objects($source, $extend) {
+    $merged = Join-Objects $source $extend
+    $extended = Add-Property $merged $extend
+    return $extended
+}
+
+# All the spec files to merge
+$specFolder = "./docs/openapi"
+$specFiles = Get-ChildItem -Path $specFolder -Filter *.json -Recurse -File -Name
+
+# Loop through all the spec files and merge them one by one
+$merged = [PSCustomObject]@{}
+foreach($specFile in $specFiles) {
+    Write-Host "Merging spec: $($specFile)" -ForegroundColor Green -NoNewline
+    try {
+        $spec = Get-Content -Path (Join-Path -Path $specFolder -ChildPath $specFile) -Raw | ConvertFrom-Json
+        $merged = Merge-Objects $spec $merged
+        Write-Host "...✅ Done!"
+    }
+    catch {
+        Write-Host "... ❌ Failed! $($_.Exception.Message)" -ForegroundColor Red
+    }
+}
+
+# Add some customizations
+$merged.info.title = "My DaprShop API"
+$merged.info.description = "My DaprShop API"
+
+# Save the merged Spec
+$destination="./DaprShop.Gateway.API/wwwroot/swagger/v1/daprshop.json"
+New-Item -ItemType File -Path $destination -Force | Out-Null
+$merged | ConvertTo-Json -Depth 100 | Out-File -FilePath $destination
--- start-dapr-sidecars.cmd
+++ start-dapr-sidecars.cmd
@@ -1,3 +1,5 @@
+pwsh merge-apis.ps1
+
 start "gateway-api" dapr run --app-port 7192 --dapr-http-port 3500 --app-ssl --components-path ./components/ --app-id gateway-api -- dotnet run -lp https --project ./DaprShop.Gateway.API/DaprShop.Gateway.API.csproj
 start "users-api" dapr run --app-port 7108 --app-ssl --components-path ./components/ --app-id users-api -- dotnet run -lp https --project ./DaprShop.UserManagement.API/DaprShop.UserManagement.API.csproj
 start "cart-api" dapr run --app-port 7283 --app-ssl --components-path ./components/ --app-id cart-api -- dotnet run -lp https --project ./DaprShop.ShoppingCart.API/DaprShop.ShoppingCart.API.csproj
