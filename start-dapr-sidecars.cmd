904d078b38730d65187d4b71264a0ddaad209cb6
William Liebenberg <awliebenberg@outlook.com>
2023-05-17 02:06:35 +1000

    fix start sidecards script

--- start-dapr-sidecars.cmd
+++ start-dapr-sidecars.cmd
@@ -6,7 +6,7 @@ start "users-api"			dapr run --app-id users-api			--app-port 7108 --app-ssl --co
 start "cart-api"			dapr run --app-id cart-api			--app-port 7283 --app-ssl --components-path ./components/ -- dotnet run -lp https --project ./DaprShop.ShoppingCart.API/DaprShop.ShoppingCart.API.csproj
 start "orders-api"			dapr run --app-id orders-api		--app-port 7233 --app-ssl --components-path ./components/ -- dotnet run -lp https --project ./DaprShop.Orders.API/DaprShop.Orders.API.csproj
 start "products-api"		dapr run --app-id products-api		--app-port 7084 --app-ssl --components-path ./components/ -- dotnet run -lp https --project ./DaprShop.Products.API/DaprShop.Products.API.csproj
-start "notifications-api"	dapr run --app-id notifications-api	--app-port 7196 --app-ssl --components-path ./components/ -- dotnet run -lp https --project ./DaprShop.Products.API/DaprShop.Products.API.csproj
+start "notifications-api"	dapr run --app-id notifications-api	--app-port 7196 --app-ssl --components-path ./components/ -- dotnet run -lp https --project ./DaprShop.Notifications.API/DaprShop.Notifications.API.csproj
 
 :: start the gateway service (ensure dapr http port is set to 3500)
 start "gateway-api"			dapr run --app-id gateway-api		--app-port 7192 --app-ssl --components-path ./components/ --dapr-http-port 3500 -- dotnet run -lp https --project ./DaprShop.Gateway.API/DaprShop.Gateway.API.csproj
