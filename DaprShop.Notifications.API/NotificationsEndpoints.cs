cafe8172624c49b73201144fb05f470456f3fc72
William Liebenberg <awliebenberg@outlook.com>
2024-01-26 20:29:03 +1100

    add build to run file

--- DaprShop.Notifications.API/NotificationsEndpoints.cs
+++ DaprShop.Notifications.API/NotificationsEndpoints.cs
@@ -12,56 +12,53 @@ public static class NotificationsEndpoints
 
 	public static void MapNotificationsEndpoints(this IEndpointRouteBuilder builder)
 	{
-		RouteGroupBuilder notifications = builder
-			.MapGroup("notifications")
-			.WithTags(new[] { "Notifications" });
+		// RouteGroupBuilder notifications = builder
+		// 	.MapGroup("notifications")
+		// 	.WithTags(new[] { "Notifications" });
 
-		notifications.MapPost("OrderCompleted", async ([FromBody] OrderCompleted orderCompletedEvent) =>
-		{
-			Console.WriteLine($"Received OrderCompleted: {orderCompletedEvent.OrderId} for {orderCompletedEvent.Username}");
-			return await Task.FromResult(Results.Ok());
-		})
-			.WithTopic(PubSubName, OrderCompletedTopic)
-			.WithName("ReceiveCompletedOrder");
-
-		
+		// notifications.MapPost("OrderCompleted", async ([FromBody] OrderCompleted orderCompletedEvent) =>
+		// {
+		// 	Console.WriteLine($"Received OrderCompleted: {orderCompletedEvent.OrderId} for {orderCompletedEvent.Username}");
+		// 	return await Task.FromResult(Results.Ok());
+		// })
+		// 	.WithTopic(PubSubName, OrderCompletedTopic)
+		// 	.WithName("ReceiveCompletedOrder");
 
 
+		// notifications.MapPost("added", ([FromBody] ProductItemAddedToCart @event) =>
+		// {
+		// 	Console.WriteLine($"---===> Item [{@event.ProductId}] added to [{@event.Username}]'s cart.");
+		// 	return Results.Ok();
+		// })
+		// .WithTopic(PubSubName, CartTopic)
+		// .WithName("ItemAdded")
+		// .ExcludeFromDescription();
 
-		notifications.MapPost("added", ([FromBody] ProductItemAddedToCart @event) =>
-		{
-			Console.WriteLine($"---===> Item [{@event.ProductId}] added to [{@event.Username}]'s cart.");
-			return Results.Ok();
-		})
-		.WithTopic(PubSubName, CartTopic)
-		.WithName("ItemAdded")
-		.ExcludeFromDescription();
+		// notifications.MapPost("removed", ([FromBody] ProductItemRemovedFromCart @event) =>
+		// {
+		// 	Console.WriteLine($"---===> Item [{@event.ProductId}] removed from [{@event.Username}]'s cart.");
+		// 	return Results.Ok();
+		// })
+		// .WithTopic(PubSubName, CartTopic)
+		// .WithName("ItemRemoved")
+		// .ExcludeFromDescription();
 
-		notifications.MapPost("removed", ([FromBody] ProductItemRemovedFromCart @event) =>
-		{
-			Console.WriteLine($"---===> Item [{@event.ProductId}] removed from [{@event.Username}]'s cart.");
-			return Results.Ok();
-		})
-		.WithTopic(PubSubName, CartTopic)
-		.WithName("ItemRemoved")
-		.ExcludeFromDescription();
+		// notifications.MapPost("cleared", ([FromBody] CartCleared @event) =>
+		// {
+		// 	Console.WriteLine($"---===> Cleared cart for [{@event.Username}]");
+		// 	return Results.Ok();
+		// })
+		// .WithTopic(PubSubName, CartTopic)
+		// .WithName("Cleared")
+		// .ExcludeFromDescription();
 
-		notifications.MapPost("cleared", ([FromBody] CartCleared @event) =>
-		{
-			Console.WriteLine($"---===> Cleared cart for [{@event.Username}]");
-			return Results.Ok();
-		})
-		.WithTopic(PubSubName, CartTopic)
-		.WithName("Cleared")
-		.ExcludeFromDescription();
-
-		notifications.MapPost("StatusChanged", ([FromBody] OrderStatusChanged @event) =>
-		{
-			Console.WriteLine($"---===> Order [{@event.OrderId}] status changed from [{@event.PreviousStatus}] to [{@event.CurrentStatus}]");
-			return Results.Ok();
-		})
-		.WithTopic(PubSubName, CartTopic)
-		.WithName("StatusChanged")
-		.ExcludeFromDescription();
+		// notifications.MapPost("StatusChanged", ([FromBody] OrderStatusChanged @event) =>
+		// {
+		// 	Console.WriteLine($"---===> Order [{@event.OrderId}] status changed from [{@event.PreviousStatus}] to [{@event.CurrentStatus}]");
+		// 	return Results.Ok();
+		// })
+		// .WithTopic(PubSubName, CartTopic)
+		// .WithName("StatusChanged")
+		// .ExcludeFromDescription();
 	}
 }
